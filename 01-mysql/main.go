0c5f01c4162e583bdfa4fe9fe06614075c8441b9
latermonk <iback.china@gmail.com>
2021-08-04 14:42:00 +0800

    mysql  query
    mysql  query  single line and multiple line

--- 01-mysql/main.go
+++ 01-mysql/main.go
@@ -11,11 +11,17 @@ import (
 // 定义一个全局对象db
 var db *sql.DB
 
+type user struct {
+	id   int
+	age  int
+	name string
+}
+
 
 // 定义一个初始化数据库的函数
 func initDB() (err error) {
 	// DSN:Data Source Name
-	dsn := "root:root1234@tcp(127.0.0.1:3306)/devops?charset=utf8mb4&parseTime=True"
+	dsn := "root:root1234@tcp(127.0.0.1:3306)/sql_test?charset=utf8mb4&parseTime=True"
 	// 不会校验账号密码是否正确
 	// 注意！！！这里不要使用:=，我们是给全局变量赋值，然后在main函数中使用全局变量db
 	db, err = sql.Open("mysql", dsn)
@@ -32,6 +38,44 @@ func initDB() (err error) {
 
 
 
+func queryRowDemo() {
+	sqlStr := "select id, name, age from user where id=?"
+	var u user
+	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
+	err := db.QueryRow(sqlStr, 1).Scan(&u.id, &u.name, &u.age)
+	if err != nil {
+		fmt.Printf("scan failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+}
+
+
+// 查询多条数据示例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	rows, err := db.Query(sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return
+	}
+	// 非常重要：关闭rows释放持有的数据库链接
+	defer rows.Close()
+
+	// 循环读取结果集中的数据
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.id, &u.name, &u.age)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			return
+		}
+		fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+	}
+}
+
+
+
 
 func main() {
 
@@ -43,6 +87,12 @@ func main() {
 		fmt.Printf("init db failed,err:%v\n", err)
 		return
 	}
+
+	//查询单行
+	queryRowDemo()
+
+	//查询多行
+	queryMultiRowDemo()
 
 
 	fmt.Println("Hello mysql")
