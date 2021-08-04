246f3ffe14d76f41c767d268d1c02c72a1b280f3
latermonk <iback.china@gmail.com>
2021-08-04 14:59:43 +0800

    Update main.go

--- 01-mysql/main.go
+++ 01-mysql/main.go
@@ -77,6 +77,59 @@ func queryMultiRowDemo() {
 
 
 
+// 插入数据
+func insertRowDemo() {
+	sqlStr := "insert into user(name, age) values (?,?)"
+	ret, err := db.Exec(sqlStr, "王五", 38)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	theID, err := ret.LastInsertId() // 新插入数据的id
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theID)
+}
+
+
+// 更新数据
+func updateRowDemo() {
+	sqlStr := "update user set age=? where id = ?"
+	ret, err := db.Exec(sqlStr, 39, 3)
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("update success, affected rows:%d\n", n)
+}
+
+
+// 删除数据
+func deleteRowDemo() {
+	sqlStr := "delete from user where id = ?"
+	ret, err := db.Exec(sqlStr, 3)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
+}
+
+
+/* Main Func  */
+
 func main() {
 
 	fmt.Println("Beginning of mysql connection ")
@@ -93,6 +146,17 @@ func main() {
 
 	//查询多行
 	queryMultiRowDemo()
+
+
+	//插入数据
+	insertRowDemo()
+
+	// 更新数据
+	updateRowDemo()
+
+
+	// 删除数据
+	deleteRowDemo()
 
 
 	fmt.Println("Hello mysql")
